e109d78082e83bd4afb172112929fc9e30a6a38e
Tobi <tobi.akiny@gmail.com>
2021-03-07 17:51:10 +0000

    Update read_exclusive_semaphore.hpp

--- read_exclusive_semaphore.hpp
+++ read_exclusive_semaphore.hpp
@@ -1,15 +1,16 @@
-struct read_write_semaphore {
+//
+// Created by tobia on 21/02/2021.
+//
+#pragma once
+
+#include <atomic>
+#include "utils.h"
+#include "event.h"
+
+class read_exclusive_semaphore {
     using read_count_t = std::atomic_unsigned_lock_free;
     static_assert(read_count_t::is_always_lock_free);
     using read_count_value_t = typename read_count_t::value_type;
-
-    inline auto exclusive_lock_free() noexcept(false) { return lock_free_exclusive_token(*this); }
-
-    inline auto exclusive_locking() { return locking_exclusive_token(*this); }
-
-    inline auto read_locking() { return read_token(*this); }
-
-private:
     event writer_done_event;
     event reader_done_event;
     event all_readers_done_event;
@@ -26,12 +27,12 @@ private:
     class max_readers_exception : public std::exception {
     };
 
-    bool reader_try_acquire() {
+    bool try_read_acquire() {
         auto expected = read_count.load();
         return expected != max_count && read_count.compare_exchange_weak(expected, expected + 1);
     }
 
-    inline bool writer_try_acquire() {
+    inline bool try_exclusive_acquire() {
         read_count_value_t expected = 0;
         return read_count.compare_exchange_weak(expected, max_count);
     }
@@ -40,7 +41,7 @@ private:
      * Could lead to exclusive accessor being starved
      */
     void exclusive_acquire() noexcept(false) {
-        all_readers_done_event.try_or_wait([this] { return writer_try_acquire(); });
+        all_readers_done_event.try_or_wait([this] { return try_exclusive_acquire(); });
     }
 
     void reader_acquire() noexcept(false) {
@@ -111,8 +112,15 @@ private:
     class lock_free_exclusive_token : public exclusive_token {
     public:
         explicit lock_free_exclusive_token(read_write_semaphore &sem) : exclusive_token(sem) {
-            if (!sem.writer_try_acquire())
+            if (!sem.try_exclusive_acquire())
                 throw acquisition_error("failed to obtain exclusive_locking access to semaphore");
         }
     };
-}
+
+public:
+    inline auto exclusive_lock_free() noexcept(false) { return lock_free_exclusive_token(*this); }
+
+    inline auto exclusive_locking() { return locking_exclusive_token(*this); }
+
+    inline auto read_locking() { return read_token(*this); }
+};
